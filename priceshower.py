ab1a4f0024f8b32a0c3f17e1574bd822642c4035
Paulo Piva <cremascopaulo@gmail.com>
2022-04-13 20:59:32 -0300

    Added BRL cotatition and users coins

--- priceshower.py
+++ priceshower.py
@@ -3,25 +3,49 @@ import requests
 import os
 import time
 
+def pricesdash(symbols,users):
 
-def pricesdash(symbols):
+  coin = 'Moeda'
+  cotdol = 'Cotação Dollar'
+  cotbrl = 'Cotação em real'
 
   while (0 == 0):  
+    #puxa a cotação do real
+    brlvalue = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL").json()
+    brlvalue = brlvalue['USDBRL']
 
+    #puxa a cotação das cripto
     priceList = []
-
     for x in symbols:
       payloads = {'symbol' : x}
-      response = requests.get("https://api.binance.com/api/v3/ticker/price" , params=payloads)
-      response = response.json()
+      response = requests.get("https://api.binance.com/api/v3/ticker/price" , params=payloads).json()
       priceList.append(response)
 
     os.system('cls')
     
+    #gera o cabeçalho da tabela
+    print(f'{coin:<15}', f'{cotdol:<18} ' , f'{cotbrl:<18}  ' ,  sep='|  ' , end='')
+    for x in users:
+      name = x['name']
+      print( '|' , f'{name:<15}' , end='')
+    print('\n---------------------------------------------------------------------------------')
+
     for x in priceList:
       symbol = x['symbol']
       price = x['price']
-      print(f'{symbol:<15}', f'{price:<18}' ,  sep='|  ')
-      print('-----------------------------------------------------------')
+      brlprice = float(price) * float(brlvalue['high'])
+
+
+      print(f'{symbol:<15}', f'${price:<18}' , f'R${brlprice:<18}'[:20] ,  sep='|  ' , end='')
+
+      for x in users:
+        try:
+          coins = str(x['coins'][symbol])
+          print( '|' , f'{coins:<15}' , end='')
+        except Exception:
+          pass
+          
+      print()
+      print('---------------------------------------------------------------------------------')
 
     time.sleep(1)
